6c505074833ea1d8beb7792ff33b3fa7a91fced3
kai-1208 <tkhski1208@gmail.com>
2025-11-08 00:46:16 +0900

    ngrokのURLを更新

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // app.js
-const API_BASE_URL = 'https://semirarely-expositional-aria.ngrok-free.dev'; // ngrokのURL
+const API_BASE_URL = 'https://10b90ecadaa3.ngrok-free.app'; // ngrokのURL
 const QUEST_API_KEY = '2025quest-api-key';
 
 // ----- フラグ定義 (フロントエンド用) -----
